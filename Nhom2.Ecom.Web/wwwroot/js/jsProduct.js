aa896e8f29790a085365fcd47f8f3625b3fab391
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2020-04-26 17:42:39 +0700

    Tạo ProductController
    Tạo trang sản phẩm chi tiết
    - Thêm chức năng comment
    
    Tách file CSS
    - styleListProd
    - styleBreadcrumb
    - styleProduct
    
    Tạo file js
    - Xử lý chọn selectbox
    - Xử lý hiện ô comment

---
+++ Nhom2.Ecom.Web/wwwroot/js/jsProduct.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('.selection').click(function () {
+        $('.selection .option').toggleClass('open');
+    });
+    $('.selection .opt-item').click(function () {
+        var val = $(this).children('span').text();
+        $('.selection .select #value').text(val);
+        // alert(val);
+    });
+
+    $('.cmt-info .rep').click(function (e) {
+        e.preventDefault();
+        $(this).parent().parent().children('.rep-cmt').toggleClass('active');
+    });
+});
